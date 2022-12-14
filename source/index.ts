c77d51307b34c6906335b5f2953c7107ebde808b
wukai <wu.vkcyan@gmail.com>
2022-12-14 13:53:47 +0800

    修改库名称 增加input 层级

--- source/index.ts
+++ source/index.ts
@@ -17,7 +17,7 @@ let defaultFastUpload = {
 export default function fastUpload(params: fastUploadType = defaultFastUpload): Promise<any> {
   return new Promise((resolve, reject) => {
     if (document.readyState != 'complete') {
-      throw new Error('dom加载异常,请确保dom完全加载再进行使用')
+      throw new Error('dom loading exception, please ensure that the dom is fully loaded before using')
     }
     let fileCancle = true // 当前input是否未上传了文件
 
@@ -35,7 +35,7 @@ export default function fastUpload(params: fastUploadType = defaultFastUpload): 
         setTimeout(() => {
           if (fileCancle) {
             removeInput(input)
-            reject('用户取消了上传')
+            reject('upload canceled')
           }
         }, 500)
       },
@@ -64,6 +64,7 @@ function createInput({ multiple, accept }: { multiple: boolean; accept: string |
   input.style.position = 'absolute'
   input.style.top = '0'
   input.style.opacity = '0'
+  input.style.zIndex = '-9999'
   input.multiple = multiple
   if (accept) {
     input.accept = accept
